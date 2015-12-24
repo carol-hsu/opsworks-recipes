52b35d3e24abd1d01616df85a0746ad88b7c957e
nosus <carol_hsu@trend.com.tw>
2015-12-24 14:50:57 +0800

    update kubernetes

--- kubernetes/recipes/flanneld.rb
+++ kubernetes/recipes/flanneld.rb
@@ -6,13 +6,14 @@ bash 'install_flannel' do
   tar zxvf flannel-0.5.2-linux-amd64.tar.gz
   cd flannel-0.5.2
   cp flanneld /usr/local/bin
+  cp mk-docker-opts.sh /opt/
   EOH
 end
 
 template "/etc/init.d/flanneld" do
 	mode "0755"
 	owner "root"
-	source "flanneld-init-conf.erb"
+	source "flanneld.erb"
 	variables ({
 	  :elb_url => node['etcd']['elb_url'],
 	  :etcd_password => node['etcd']['password']
