97a50375d97beb0f02d85c78b5fb73433079e5ec
nosus <carol_hsu@trend.com.tw>
2015-12-24 15:21:07 +0800

    update minion

--- kubernetes/recipes/kubernetes-minion-setup.rb
+++ kubernetes/recipes/kubernetes-minion-setup.rb
@@ -1,9 +1,23 @@
+include_recipe 'kubernetes::kubernetes'
+
+bash "minion-file-copy" do
+    user 'root'
+    cwd '/tmp//kubernetes/server/kubernetes/server/bin'
+    code <<-EOH
+    mkdir /var/lib/kubelet
+    mkdir /etc/kubernetes
+    cp kubelet kube-proxy /usr/local/bin/
+    EOH
+end
+
 template "/etc/init.d/kubernetes-minion" do
 	mode "0755"
 	owner "root"
 	source "kubernetes-minion.erb"
-	variables :master_url => my_master_elb[:dns_name]
+	variables :master_url => node['kubernetes']['master_url']
+	subscribes :create, "bash[minion-file-copy]", :immediately
 	notifies :disable, 'service[kubernetes-minion]', :delayed
+    action :nothing
 end
 
 service "kubernetes-minion" do
