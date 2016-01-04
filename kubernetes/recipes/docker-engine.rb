9c687a6b34ef4b4633baea4c01a30b31fdf004e9
nosus <carol_hsu@trend.com.tw>
2016-01-04 17:21:02 +0800

    add docker-engine installation

---
+++ kubernetes/recipes/docker-engine.rb
@@ -0,0 +1,18 @@
+template "/etc/yum.repos.d/docker.repo" do
+	mode "0644"
+    owner "root"
+    source "docker.repo.erb"
+    notifies :install, "package[docker-engine]", :immediately
+end
+
+package 'docker-engine' do
+	action :nothing
+	notifies :create, "template[/etc/sysconfig/docker]", :immediately
+end
+
+template "/etc/sysconfig/docker" do
+	mode "0644"
+	owner "root"
+	source "docker.erb"
+	action :nothing
+end
