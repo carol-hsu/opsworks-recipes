a6e1647354b698fd7024af297e3f696bcc4b5a0b
nosus <carol_hsu@trend.com.tw>
2016-04-06 14:43:11 +0800

    update kubelet with dns setting

---
+++ kubernetes/recipes/kubelet-enable-dns.rb
@@ -0,0 +1,15 @@
+# enable DNS setting in kubelet and restart 
+include_recipe 'kubernetes::kubernetes-minion-run'
+
+execute 'add-dns-settings' do
+	command <<-EOH
+    sed -i '41c \        \--cluster-domain=#{node['kubernetes']['dns_domain']}' /etc/init.d/kubernetes-minion
+    sed -i '41c \        \--cluster-dns=#{node['kubernetes']['dns_ip']}' /etc/init.d/kubernetes-minion
+	EOH
+	not_if do "fgrep 'cluster-dns' /etc/init.d/kubernetes-minion" end
+    notifies :restart, 'service[kubernetes-minion]', :delayed
+end
+
+service "kubernetes-minion" do
+	action: nothing
+end
---
+++ kubernetes/recipes/monitoring-cluster-setup.rb
@@ -0,0 +1,50 @@
+# run the pod and servie of monitoring system
+include_recipe 'kubernetes::kubernetes-master-run'
+unless Dir.exist?('/opt/monitoring-template')
+	directory "/opt/monitoring-template" do
+		 owner 'root'
+    	 group 'root'
+    	 mode '0755'
+    	 action :create
+    	 notifies :create, 'template[/opt/monitoring-template/.yaml]', :immediately
+	end
+end
+
+template "/opt/dns-template/skydns-rc.yaml" do
+    mode "0644"
+    owner "root"
+    source "skydns-rc.yaml.erb"
+    variables :dns_domain => node['kubernetes']['dns_domain']
+    notifies :run, "execute[wait_apiserver_running]", :delayed
+    action :nothing
+end
+
+template "/opt/dns-template/skydns-svc.yaml" do
+    mode "0644"
+    owner "root"
+    source "skydns-svc.yaml.erb"
+    variables :dns_ip => node['kubernetes']['dns_ip']
+    notifies :run, "execute[wait_apiserver_running]", :delayed
+    action :nothing
+end
+
+execute 'wait_apiserver_running' do
+	command "sleep 10"
+    action :nothing
+    notifies :run, 'execute[run-rc]', :delayed
+    notifies :run, 'execute[run-svc]', :delayed
+end
+
+
+execute "run-rc" do
+    cwd "/opt/dns-template/"
+    command "kubectl create -f skydns-rc.yaml"
+    action :nothing
+end
+
+execute "run-svc" do
+    cwd "/opt/dns-template/"
+    command "kubectl create -f skydns-svc.yaml"
+    action :nothing
+end
+
