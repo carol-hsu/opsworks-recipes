0ffada73d84f3d7119c305fc4a502ba14a83a022
nosus <carol_hsu@trend.com.tw>
2016-04-07 11:12:28 +0800

    fix sed error

--- kubernetes/recipes/kubelet-enable-dns.rb
+++ kubernetes/recipes/kubelet-enable-dns.rb
@@ -3,8 +3,8 @@ include_recipe 'kubernetes::kubernetes-minion-run'
 
 execute 'add-dns-settings' do
 	command <<-EOH
-    sed -i '41a \\        \\--cluster-domain=#{node['kubernetes']['dns_domain']} \\\' /etc/init.d/kubernetes-minion
-    sed -i '41a \\        \\--cluster-dns=#{node['kubernetes']['dns_ip']} \\\' /etc/init.d/kubernetes-minion
+	sed -i "41a \\                --cluster-domain=#{node['kubernetes']['dns_domain']} \\\\" /etc/init.d/kubernetes-minion
+	sed -i "41a \\                --cluster-dns=#{node['kubernetes']['dns_ip']} \\\\" /etc/init.d/kubernetes-minion
 	EOH
     notifies :restart, 'service[restart-kubernetes-minion]', :delayed
 end
