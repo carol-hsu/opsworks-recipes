b780419f990c62750ddfdf30ff79bd880559d41e
nosus <carol_hsu@trend.com.tw>
2016-04-13 12:10:41 +0800

    change to double quotes

--- kubernetes/recipes/docker-registry-auth.rb
+++ kubernetes/recipes/docker-registry-auth.rb
@@ -1,4 +1,5 @@
 execute 'generate-auth' do
     user 'root'
-	command 'docker login -u #{node['docker']['user']} -p #{node['docker']['password']} -e yoyo@trend.com #{node['docker']['registry']}'
+	command "docker login -u #{node['docker']['user']} -p #{node['docker']['password']} -e yoyo@trend.com #{node['docker']['registry']}"
+
 end
--- kubernetes/recipes/kubelet-registry-auth.rb
+++ kubernetes/recipes/kubelet-registry-auth.rb
@@ -5,6 +5,8 @@ bash 'copy-auth' do
     cwd '/var/lib/kubelet'
     code <<-EOH
       cp ~/.docker/config ./.dockercfg
-	  
+	  sed -i '1d' ./.dockercfg  
+	  sed -i '1d' ./.dockercfg  
+	  sed -i '$d' ./.dockercfg  
     EOH
 end
