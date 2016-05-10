779f6e5ef69cc87a00f0ca754d9325999d72ae9d
nosus <carol_hsu@trend.com.tw>
2016-05-10 16:38:31 +0800

    update strict condition while setup

--- kubernetes/recipes/kubernetes-master-setup.rb
+++ kubernetes/recipes/kubernetes-master-setup.rb
@@ -2,15 +2,27 @@ include_recipe 'kubernetes::kubernetes'
 
 bash "master-file-copy" do
     user 'root'
-    cwd '/tmp'
+    cwd '/tmp/kubernetes/server/kubernetes/server/bin'
     code <<-EOH
-    if ! [[ $(ls /usr/local/bin/kube*) ]]; then
-      mkdir /etc/kubernetes
-	  cd ./kubernetes/server/kubernetes/server/bin
-      cp kubectl kube-apiserver kube-scheduler kube-controller-manager kube-proxy /usr/local/bin/
-    fi
+    if [[ $(ls /usr/local/bin/kubectl) ]]; then
+		current_version=$(/usr/local/bin/kubectl version | awk 'NR==1' | awk -F":\"v" '{ print $2 }' | awk -F"\"," '{ print $1 }')		
+		if [ "$current_version" -eq "#{node['kubernetes']['version']}" ]; then
+        	echo "Passed binary file copy, the version is already updated"
+        	exit
+    	fi
+	fi
+    cp kubectl kube-apiserver kube-scheduler kube-controller-manager kube-proxy /usr/local/bin/
     EOH
 end
+
+directory '/etc/kubernetes' do
+	owner 'root'
+	group 'root'
+	mode '0755'
+	subscribes :create, "bash[master-file-copy]", :immediately
+    action :nothing
+end
+
 
 etcd_endpoint="http://root:#{node['etcd']['password']}@#{node['etcd']['elb_url']}:80"
 
@@ -34,5 +46,4 @@ file "/root/ba_file" do
 	content "#{node['ba']['password']},#{node['ba']['account']},#{node['ba']['uid']}"
 	action :create
 end
-
 
