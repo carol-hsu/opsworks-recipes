75ebd6896f983b4b1e4fba2c1b6f7868db979190
nosus <carol_hsu@trend.com.tw>
2016-09-15 19:32:48 +0800

    etcd-clustering

--- kubernetes/recipes/etcd-clustering.rb
+++ kubernetes/recipes/etcd-clustering.rb
@@ -1,14 +1,31 @@
-private_ip = node['opsworks']['instance']['private_ip']
-hostname = node['opsworks']['instance']['hostname']
+chef_version = Chef::VERSION
+major_version = chef_version.split(".")[0]
+
 members = Array.new
 ip_enable_ba = nil
 
-node['opsworks']['layers']['etcd']['instances'].each do |inst|
-	members << inst[0]+"=http://"+inst[1][:private_ip]+":2380"
-	if ip_enable_ba == nil
-		ip_enable_ba = inst[1][:private_ip]
-	end
+if major_version == "11"
+    node['opsworks']['layers']['etcd']['instances'].each do |inst|
+	    members << inst[0]+"=http://"+inst[1][:private_ip]+":2380"
+	    if ip_enable_ba == nil
+		    ip_enable_ba = inst[1][:private_ip]
+	    end
+    end
+    instance = node['opsworks']['instance']
+
+else #Chef 12
+    search(:node, "name:etcd*").each do |inst|
+        members << inst['hostname']+"=http://"+inst['ipaddress']+":2380"
+        if ip_enable_ba == nil
+            ip_enable_ba = inst['ipaddress']
+        end
+    end
+    instance = search("aws_opsworks_instance", "self:true").first
+
 end
+
+private_ip = instance['private_ip']
+hostname = instance['hostname']
 
 template "/root/etcd_static_bootstrap.sh" do
 	mode "0755"
