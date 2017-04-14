291bf4d87109613c45099a13469a5010a1157ef9
Ke-Jou Hsu <carol-hsu@users.noreply.github.com>
2017-04-14 11:57:14 +0800

    Update slave-setup.rb
    change to JAVA 1.8 for avoid the bug: https://issues.jenkins-ci.org/browse/JENKINS-43492

--- jenkins/recipes/slave-setup.rb
+++ jenkins/recipes/slave-setup.rb
@@ -19,9 +19,18 @@ bash 'install-ant' do
 	user 'root'
 	code <<-EOH
 	yum -y update
-	yum -y install java
+        # yum -y install java
 	yum -y install ant
-	EOH
+        # install Java 1.8
+        cd /opt/
+        wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-linux-x64.tar.gz"
+        tar xzf jdk-8u121-linux-x64.tar.gz
+        alternatives --install /usr/bin/java java /opt/jdk1.8.0_121/bin/java 1
+        alternatives --install /usr/bin/javac javac /opt/jdk1.8.0_121/bin/javac 1
+        alternatives --install /usr/bin/jar jar /opt/jdk1.8.0_121/bin/jar 1
+        alternatives --set jar /opt/jdk1.8.0_121/bin/jar
+        alternatives --set javac /opt/jdk1.8.0_121/bin/javac
+        EOH
 end
 
 #install docker
