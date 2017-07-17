09783e35f3fbd68cf21a90f8dc4230b3d0bd4a7e
nosus <nosus@tomofuns-MacBook-Pro.local>
2017-07-17 14:17:42 +0800

    update java sdk installation

--- jenkins/recipes/slave-setup.rb
+++ jenkins/recipes/slave-setup.rb
@@ -15,17 +15,13 @@ directory '/home/jenkins/.ssh' do
 	mode '0700'
 end
 
-bash 'install-ant' do
+bash 'install-ant-and-java' do
 	user 'root'
 	code <<-EOH
 	yum -y update
-    # yum -y install java
 	yum -y install ant
-    # install Java 1.8
-    cd /opt/
-    wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-linux-x64.rpm
-    yum -y localinstall jdk-8u121-linux-x64.rpm
-        
+	yum install java-1.8.0
+	yum remove java-1.7.0-openjdk
     EOH
 end
 
